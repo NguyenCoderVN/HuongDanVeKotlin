7cd35c0267347dd92aae4fed6feef502cf09e173
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-02-19 19:49:21 +0700

    Commit Kiểu dử liệu, biến và toán tử

--- src/main/kotlin/Phan01/Main.kt
+++ src/main/kotlin/Phan01/Main.kt
@@ -2,4 +2,30 @@ package Phan01
 
 fun main(args: Array<String>) {
     println("Hello Kotlin")
+    var x = 10
+    var y = x + 10
+    println("Giá trị của y là $y")
+
+    var a = 10.25
+    println("Giá trị của a là $a")
+    var b: Float = a.toFloat()
+    println("Giá trị của b là $b")
+    var c:Float = b
+    //var c:Float = a
+
+    var text = "a"
+    //var text1:Char = text
+    var text1:String = text
+
+    var text02 =  "abc"
+    println("Ký tự đầu của text02 là ${text02[0]}")
+
+    var myArray : Array<Long> = arrayOf(15,16,20)
+    var testInt:Int = myArray[2].toInt()
+    println("Giá trị của testInt là $testInt")
+
+    val d = 10
+    //d = 15
+
+
 }
