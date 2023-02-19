da70572aebbd185475923b11fd3ddc65af2dedb0
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-02-19 21:08:34 +0700

    Commit Vòng lặp và rẽ nhánh

--- src/main/kotlin/Phan01/Main.kt
+++ src/main/kotlin/Phan01/Main.kt
@@ -27,5 +27,56 @@ fun main(args: Array<String>) {
     val d = 10
     //d = 15
 
+//    println("Mời bạn nhập một số")
+//    var enterInt : Int = readLine()!!.toInt()
+//    if (enterInt % 2 == 0) {
+//        println("Số bạn vừa nhập là số chẳn")
+//    } else {
+//        println("Số bạn vừa nhập là số lẻ")
+//    }
+
+    val day = 7
+    when (day) {
+        2 -> println("Thứ 2")
+        3 -> println("Thứ 3")
+        4 -> println("Thứ 4")
+        5 -> println("Thứ 5")
+        6 -> println("Thứ 6")
+        7 -> println("Thứ 7")
+        8 -> println("Chủ nhật")
+        else -> println("Ngày không hợp lệ")
+    }
+
+    val testFor = 5
+
+    for (i in 1..testFor) {
+        println("Giá trị i là $i")
+    }
+
+    for (i in 1 until testFor) {
+        println("Giá trị i là $i")
+    }
+
+    for (i in testFor downTo  1) {
+        println("Giá trị i là $i")
+    }
+
+    val array01 = arrayOf("Một","Hai","Ba","Bốn")
+    for (item in array01) {
+        println("Giá trị item là $item")
+    }
+
+    var itemWhile = 1
+    while (itemWhile<=10) {
+        println("Giá trị là $itemWhile")
+        itemWhile++
+    }
+
+    var itemDoWhile = 1
+    do {
+        println("Giá trị là $itemDoWhile")
+        itemDoWhile++
+    } while (itemDoWhile<=0)
+
 
 }
